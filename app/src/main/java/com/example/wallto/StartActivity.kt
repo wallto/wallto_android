a12d91bbfdb8b76d12fde6315ab354ddb85cc658
Mpk <bartmoretti@gmail.com>
2019-05-15 19:12:51 +0300

    Add dependencies, start screen

--- app/src/main/java/com/example/wallto/StartActivity.kt
+++ app/src/main/java/com/example/wallto/StartActivity.kt
@@ -3,10 +3,10 @@ package com.example.wallto
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class StartActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_start)
     }
 }
