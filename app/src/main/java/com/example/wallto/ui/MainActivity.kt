727fa99fec1e5135bf7a284b4ae4b8ce11021da3
Mpk <bartmoretti@gmail.com>
2019-05-17 10:58:51 +0300

    Add icons, splash screen, main activity mock

---
+++ app/src/main/java/com/example/wallto/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.wallto.ui
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wallto.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
--- app/src/main/java/com/example/wallto/ui/StartActivity.kt
+++ app/src/main/java/com/example/wallto/ui/StartActivity.kt
@@ -1,7 +1,9 @@
 package com.example.wallto.ui
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.wallto.R
 
 class StartActivity : AppCompatActivity() {
@@ -10,5 +12,12 @@ class StartActivity : AppCompatActivity() {
         setTheme(R.style.AppTheme_NoActionBar)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+
+        val button = findViewById<Button>(R.id.btnAuth)
+
+        button.setOnClickListener {
+            val intent = Intent(this@StartActivity, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
